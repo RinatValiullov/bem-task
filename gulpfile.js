342948cd9d7598e85599d0469ecc708605459cd8
RinatValiullov <4rontender@gmail.com>
2019-01-28 20:58:55 +0500

    Intermediate result

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,51 @@
-const { src, dest, watch, series } = require("gulp");
 let sass = require("gulp-sass");
+let postcss = require("gulp-postcss");
+let autoprefixer = require("autoprefixer");
+let concatcss = require("gulp-concat-css");
+let {
+    src,
+    dest,
+    watch,
+    series
+} = require("gulp");
 
 let browserSync = require("browser-sync").create();
 
 function browserSyncFunc() {
-  browserSync.init({
-    server: {
-      baseDir: "./common.blocks/button/"
-    },
-    notify: false
-  });
+    browserSync.init({
+        server: {
+            baseDir: "./common.blocks/button/"
+        },
+        notify: false
+    });
 }
 
 // Static Server + watching scss/html files
 function serve() {
-  browserSyncFunc();
+    browserSyncFunc();
 
-  watch("./common.blocks/button/**/*.scss", series(sassCompile));
-  watch(
-    "./common.blocks/button/**/*.html",
-    { events: "change" },
-    browserSync.reload
-  );
+    watch("./common.blocks/**/*.scss", series(sassCompile));
+    watch(
+        "./common.blocks/button/**/*.html",
+        browserSync.reload
+    );
 }
 
 // Compile sass into CSS & auto-inject into browsers
 function sassCompile() {
-  return src("./common.blocks/button/**/*.scss")
-    .pipe(sass())
-    .pipe(dest("./common.blocks/button/css/"))
-    .pipe(browserSync.stream());
+
+    let plugins = [
+        autoprefixer({
+            browsers: ["last 4 versions", "not ie < 11"]
+        })
+    ]
+
+    return src("./common.blocks/**/*.scss")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concatcss('ui.css'))
+        .pipe(postcss(plugins))
+        .pipe(dest("./common.blocks/button/css/"))
+        .pipe(browserSync.stream());
 }
 
 exports.browserSyncFunc = browserSyncFunc;
